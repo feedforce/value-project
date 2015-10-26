7678b6616a483347bb2a90acf35154434e70865e
mgi166 <skskoari@gmail.com>
2015-10-27 08:38:23 +0900

    Add ValueCaster::Action::Base class
    * you must define `call` method

--- lib/value_caster/action/base.rb
+++ lib/value_caster/action/base.rb
@@ -1,6 +1,15 @@
 module ValueCaster
   module Action
     class Base
+      def initialize(data)
+        @data = data
+      end
+
+      attr_accessor :data
+
+      def call
+        raise NotImplementedError
+      end
     end
   end
 end
