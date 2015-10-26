a9f26d501d09c6c86143a7c40fc26ba60975ab5a
mgi166 <skskoari@gmail.com>
2015-10-26 15:51:53 +0900

    Bot#start call Slack::Client::RealTime#start

--- lib/value_caster/bot.rb
+++ lib/value_caster/bot.rb
@@ -1,4 +1,12 @@
 module ValueCaster
   class Bot
+    def initialize(options = {})
+      @client = Slack::Client.new(options)
+      @rtm    = Slack::Client.new(options).realtime
+    end
+
+    def start
+      @rtm.start
+    end
   end
 end
