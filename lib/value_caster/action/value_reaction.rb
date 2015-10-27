8511f830fbaf846d0a0ce96253272fe8abd2a8fd
mgi166 <skskoari@gmail.com>
2015-10-27 10:45:27 +0900

    Add `append_to_spread_sheet`
    * Append data to google spread sheet.
      * To save data permanently
    * Move from bin/caster

--- lib/value_caster/action/value_reaction.rb
+++ lib/value_caster/action/value_reaction.rb
@@ -1,3 +1,7 @@
+require 'hashie/mash'
+require "google/api_client"
+require "google_drive"
+
 module ValueCaster
   module Action
     class ValueReaction < Base
@@ -9,6 +13,7 @@ module ValueCaster
         return if data.reaction != 'value'
 
         post_to_slack(data)
+        append_to_spread_sheet(data)
       end
 
       def post_to_slack(data)
@@ -28,6 +33,28 @@ module ValueCaster
         )
       end
 
+      def append_to_spread_sheet(data)
+        user_id = data.user
+
+        reacted_username = slack_username(user_id)
+        reacted_message  = reacted_message(user_id)
+
+        row = [
+          reacted_message.username,
+          reacted_message.text,
+          reacted_message.permalink,
+          reacted_username,
+          reacted_message.count,
+          reacted_message.timestamp,
+        ]
+
+        SpreadSheet.update_lock do
+          spread_sheet = SpreadSheet.new
+          spread_sheet.append(row)
+          spread_sheet.save
+        end
+      end
+
       def reacted_message(user_id)
         reactions = @client.reactions_list(user: user_id)
         reactions = Hashie::Mash.new(reactions)
@@ -39,7 +66,58 @@ module ValueCaster
         Hashie::Mash.new(user_info).user.name
       end
 
-      class SlackMessage < Hashie::Mash
+      class SlackMessage < ::Hashie::Mash
+        def count
+          reactions.find {|react| react.name == 'value' }.count
+        end
+
+        def timestamp
+          ts.to_i.strftime('%Y/%m/%d %H:%M:%S')
+        end
+      end
+
+      class SpreadSheed
+        SHEET_ID = '18DwKbuMfj22H9XWm-HigilxZHtFa_n8F0fwB_K-fNnE'
+
+        def initialize
+          signet = Signet::OAuth2::Client.new(
+            client_id: ENV['GOOGLE_CLIENT_ID'],
+            client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+            authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+            token_credential_uri: 'https://accounts.google.com/o/oauth2/token',
+            scope: [ "https://www.googleapis.com/auth/drive" ],
+            refresh_token: ENV['GOOGLE_REFRESH_TOKEN']
+          )
+
+          signet.refresh!
+          @work_sheet = GoogleDrive.login_with_oauth(signet.access_token)
+                          .spreadsheet_by_key(SHEET_ID)
+                          .worksheets[0]
+        end
+
+        attr_reader :work_sheet
+
+        def self.update_lock(&block)
+          Redis::Lock.new('google-drive-update', timeout: 10).lock do
+            yield
+          end
+        end
+
+        def append(data)
+          data.each.with_index(1) do |d, i|
+            @work_sheet[max_row_size + 1, i] = d
+          end
+        end
+
+        def save
+          @work_sheet.save
+        end
+
+        private
+
+        def max_row_size
+          @max_row_size ||= @work_sheet.rows.size
+        end
       end
     end
   end
