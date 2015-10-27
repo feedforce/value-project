147a2164e677363467e17780f54924c8becbd031
mgi166 <skskoari@gmail.com>
2015-10-27 10:36:42 +0900

    Post to slack in ValueReaction

--- lib/value_caster/action/value_reaction.rb
+++ lib/value_caster/action/value_reaction.rb
@@ -6,6 +6,40 @@ module ValueCaster
       end
 
       def call(data)
+        return if data.reaction != 'value'
+
+        post_to_slack(data)
+      end
+
+      def post_to_slack(data)
+        user_id = data.user
+
+        reacted_username = slack_username(user_id)
+        reacted_message  = reacted_message(user_id)
+
+        text = "#{reacted_username} さんが ナイス value! と言っています\n> #{reacted_message.permalink}"
+
+        @client.chat_postMessage(
+          channel: ENV['SLACK_NOTIFICATION_CHANNEL'],
+          text: text,
+          username: 'valueくん',
+          icon_emoji: ':value:',
+          unfurl_links: true
+        )
+      end
+
+      def reacted_message(user_id)
+        reactions = @client.reactions_list(user: user_id)
+        reactions = Hashie::Mash.new(reactions)
+        SlackMessage.new(reactions.items.first.message)
+      end
+
+      def slack_username(user_id)
+        user_info = @client.users_info(user: user_id)
+        Hashie::Mash.new(user_info).user.name
+      end
+
+      class SlackMessage < Hashie::Mash
       end
     end
   end
