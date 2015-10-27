217a3afaacc9be6b32fbeb2e4f1318db8582534d
mgi166 <skskoari@gmail.com>
2015-10-27 10:52:29 +0900

    require 'value_caster/action'

--- lib/value_caster.rb
+++ lib/value_caster.rb
@@ -3,6 +3,7 @@ require 'value_caster/bot'
 require 'value_caster/event'
 require 'value_caster/event_router'
 require 'value_caster/event_register'
+require 'value_caster/action'
 
 module ValueCaster
   # Your code goes here...
