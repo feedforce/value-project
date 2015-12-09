43790e298add3c4fd85bdc29330bf53c4f6d0b8c
mgi166 <skskoari@gmail.com>
2015-12-09 11:23:23 +0900

    renamed: lib/value_caster.rb -> lib/value_project.rb

--- lib/value_project.rb
+++ lib/value_project.rb
@@ -2,20 +2,20 @@ require 'hashie/mash'
 require 'active_support'
 require 'active_support/core_ext'
 require 'slack'
-require "value_caster/version"
-require 'value_caster/concern/slack_channel'
-require 'value_caster/bot'
-require 'value_caster/event'
-require 'value_caster/event_router'
-require 'value_caster/event_register'
-require 'value_caster/action'
-require "value_caster/logger"
+require "value_project/version"
+require 'value_project/concern/slack_channel'
+require 'value_project/bot'
+require 'value_project/event'
+require 'value_project/event_router'
+require 'value_project/event_register'
+require 'value_project/action'
+require "value_project/logger"
 require 'redis/objects'
 require 'redis/lock'
 
 Redis.current = Redis.new(url: ENV["REDISTOGO_URL"] || 'redis://localhost:6379/0')
 
-ValueCaster::EventRouter.draw do
+ValueProject::EventRouter.draw do
   map :reaction_added,  action: :value_reaction
   map :channel_created, action: :bot_invitation
 end
