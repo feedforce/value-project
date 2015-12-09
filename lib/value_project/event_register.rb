1b27873f68ebff462fc70d233401b5e83e5a5efe
mgi166 <skskoari@gmail.com>
2015-12-09 13:33:32 +0900

    Rescue action exception
    * Prevent from termination the program

--- lib/value_project/event_register.rb
+++ lib/value_project/event_register.rb
@@ -11,11 +11,21 @@ module ValueProject
     def regist
       ValueProject::EventRouter.event_collection.each do |name, event|
         @rtm.on(name) do |data|
-          Logger.logger.info "Emit #{name} event, #{event.action_class} is going to call with: #{data.to_hash}."
-          event.action_class.new.call(Hashie::Mash.new(data))
-          Logger.logger.info "Finish #{name} event, #{event.action_class} is called with: #{data.to_hash}."
+          begin
+            logger.info "Emit #{name} event, #{event.action_class} is going to call with: #{data.to_hash}."
+            event.action_class.new.call(Hashie::Mash.new(data))
+            logger.info "Finish #{name} event, #{event.action_class} is called with: #{data.to_hash}."
+          rescue
+            logger.error "Failed to #{name} action with #{data}.\nbacktrace: #{$!.backtrace.join("\n")}"
+          end
         end
       end
+    end
+
+    private
+
+    def logger
+      Logger.logger
     end
   end
 end
